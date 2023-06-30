8d228c0d5a537b200785e77dfd36d8c7277a229b
GO V IND <govind123.ga@gmail.com>
2023-07-01 01:00:05 +0530

    Update Amazon_review.py

--- Amazon_review.py
+++ Amazon_review.py
@@ -8,7 +8,7 @@ response = requests.get(url)
 soup = BeautifulSoup(response.text, "html.parser")
 elements = soup.find_all(class_="a-fixed-right-grid-col a-col-left")
 text_content = [element.get_text(strip=True) for element in elements]
-with open("reviews.txt", "w", encoding="utf-8") as file:
+with open("reviews1.txt", "w", encoding="utf-8") as file:
     file.write("\n".join(text_content))
 
-print("Reviews saved to 'reviews.txt'.")
+print("Reviews saved to 'reviews1.txt'.")
